70e467f740d8731e12d9100cb8a04795afd14479
github username <github email address>
2021-03-21 18:25:54 -0400

    1st deliverable

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
-import React from "react";
+
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
+
+
 function App() {
+
+  const[plants, setPlants]= useState([])
+ 
+useEffect (
+  () => {
+    fetch("http://localhost:6001/plants")
+    .then(response => response.json())
+    .then(newPlants => {
+      setPlants(newPlants)
+    })
+  }, []
+)
+
+
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+      <PlantPage plants={plants}/>
     </div>
   );
 }
